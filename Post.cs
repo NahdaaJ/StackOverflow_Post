088b62a247b95a06f20bcf8ad611146ba30b8ff5
Nahdaa <jnahdaa@gmail.com>
2023-05-07 21:37:51 +0100

    Creating a script to mimic a StackOverflow post.

---
+++ Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow_Post
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        private int _vote = 0;
+
+        
+        public void DisplayPost()
+        {
+            Console.WriteLine("\nTitle: "+ Title);
+            Console.WriteLine("Posted on: " + DateTime.Now);
+            Console.WriteLine(Description);
+        }
+
+        public void Upvote()
+        {
+            _vote++;
+        }
+
+        public void Downvote()
+        {
+            _vote--;
+        }
+
+        public int ViewVotes()
+        {
+            return _vote;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow_Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newPost = new Post();
+            Console.WriteLine("Please enter the title of your post: ");
+            newPost.Title = Console.ReadLine();
+            Console.WriteLine("\nPlease enter the content of your post: ");
+            newPost.Description = Console.ReadLine();
+
+            newPost.DisplayPost();
+
+            while(true)
+            {
+                Console.Write("Upvote (u), Downvote (d) or View Votes (v): ");
+                var userInput = Console.ReadLine().ToLower();
+
+                if(userInput == "u")
+                {
+                    newPost.Upvote();
+                    Console.WriteLine("Upvoted.");
+                }
+                else if(userInput == "d")
+                {
+                    newPost.Downvote();
+                    Console.WriteLine("Downvoted.");
+                }
+                else if (userInput == "v")
+                {
+                    newPost.DisplayPost();
+                    Console.WriteLine("Votes: "+ newPost.ViewVotes());
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input.");
+                }
+            }
+        }
+    }
+}
